9365d3ca3625424030d46af3cabf868f2bff4548
asakaplan <you@example.com>
2015-09-12 17:34:01 -0400

    Implemented login

--- app/src/main/java/edu/vcu/ramhacks/activities/LoginActivity.java
+++ app/src/main/java/edu/vcu/ramhacks/activities/LoginActivity.java
@@ -1,11 +1,14 @@
 package edu.vcu.ramhacks.activities;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import edu.vcu.ramhacks.R;
+import edu.vcu.ramhacks.fragments.SignInFragment;
+import edu.vcu.ramhacks.utils.Constants;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +16,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // the sign in fragment is always first
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container, new SignInFragment(), Constants.TAG_FRAGMENT);
+        fragmentTransaction.commit();
     }
 
     @Override
