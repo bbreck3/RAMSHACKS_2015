be83df7711aa2f85b63d4467727783c0ec0e4a37
Hawkings <daniel.marin.94@gmail.com>
2015-09-13 03:16:31 -0400

    add banking age test

---
+++ app/src/androidTest/java/edu/vcu/ramhacks/BankValidatorTest.java
@@ -0,0 +1,7 @@
+package edu.vcu.ramhacks;
+
+/**
+ * Created by Dani on 13/09/2015.
+ */
+public class BankValidatorTest {
+}
---
+++ app/src/main/java/edu/vcu/ramhacks/utils/BankCallback.java
@@ -0,0 +1,7 @@
+package edu.vcu.ramhacks.utils;
+
+/**
+ * Created by Dani on 12/09/2015.
+ */
+public interface BankCallback {
+}
