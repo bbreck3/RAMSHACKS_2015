a651c932f73557c6eb5ffb02c721d1e68c08268e
mightyheretic <mightyheretic@gmail.com>
2015-09-13 13:48:02 -0400

    Update EEGFragment.java
    EEG

--- app/src/main/java/edu/vcu/ramhacks/fragments/EEGFragment.java
+++ app/src/main/java/edu/vcu/ramhacks/fragments/EEGFragment.java
@@ -1,36 +1,389 @@
 package edu.vcu.ramhacks.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.interaxon.libmuse.Accelerometer;
+import com.interaxon.libmuse.AnnotationData;
+import com.interaxon.libmuse.ConnectionState;
+import com.interaxon.libmuse.Eeg;
+import com.interaxon.libmuse.LibMuseVersion;
+import com.interaxon.libmuse.MessageType;
+import com.interaxon.libmuse.Muse;
+import com.interaxon.libmuse.MuseArtifactPacket;
+import com.interaxon.libmuse.MuseConfiguration;
+import com.interaxon.libmuse.MuseConnectionListener;
+import com.interaxon.libmuse.MuseConnectionPacket;
+import com.interaxon.libmuse.MuseDataListener;
+import com.interaxon.libmuse.MuseDataPacket;
+import com.interaxon.libmuse.MuseDataPacketType;
+import com.interaxon.libmuse.MuseFileFactory;
+import com.interaxon.libmuse.MuseFileReader;
+import com.interaxon.libmuse.MuseFileWriter;
+import com.interaxon.libmuse.MuseManager;
+import com.interaxon.libmuse.MusePreset;
+import com.interaxon.libmuse.MuseVersion;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import edu.vcu.ramhacks.R;
 
-public class EEGFragment extends Fragment {
+public class EEGFragment extends Fragment implements OnClickListener {
 
+    public int index;
+    public boolean full;
+    public double alpha[];
+    public double beta[];
+    public double gamma[];
+    public double theta[];
+    private Muse muse = null;
+    private ConnectionListener connectionListener = null;
+    private DataListener dataListener = null;
+    private boolean dataTransmission = true;
+    private MuseFileWriter fileWriter = null;
+
+    class ConnectionListener extends MuseConnectionListener {
+
+        final WeakReference<EEGFragment> fragmentRef;
+
+        ConnectionListener(final WeakReference<EEGFragment> fragmentRef) {
+            this.fragmentRef = fragmentRef;
+        }
+
+        @Override
+        public void receiveMuseConnectionPacket(MuseConnectionPacket p) {
+            final ConnectionState current = p.getCurrentConnectionState();
+            final String status = p.getPreviousConnectionState().toString() +
+                    " -> " + current;
+            final String full = "Muse " + p.getSource().getMacAddress() +
+                    " " + status;
+            Log.i("Muse Headband", full);
+            EEGFragment fragment = fragmentRef.get();
+            // UI thread is used here only because we need to update
+            // TextView values. You don't have to use another thread, unless
+            // you want to run disconnect() or connect() from connection packet
+            // handler. In this case creating another thread is required.
+            if (fragment != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView statusText =
+                                (TextView) getView().findViewById(R.id.con_status);
+                        statusText.setText(status);
+                        if (current == ConnectionState.CONNECTED) {
+                            MuseVersion museVersion = muse.getMuseVersion();
+                            String version = museVersion.getFirmwareType() +
+                                    " - " + museVersion.getFirmwareVersion() +
+                                    " - " + Integer.toString(
+                                    museVersion.getProtocolVersion());
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    class DataListener extends MuseDataListener {
+
+        final WeakReference<EEGFragment> fragmentRef;
+        private MuseFileWriter fileWriter;
+
+        DataListener(final WeakReference<EEGFragment> fragmentRef) {
+            this.fragmentRef = fragmentRef;
+        }
+
+        @Override
+        public void receiveMuseDataPacket(MuseDataPacket p) {
+            switch (p.getPacketType()) {
+                case EEG:
+                    updateEeg(p.getValues());
+                    break;
+                case ACCELEROMETER:
+                    updateAccelerometer(p.getValues());
+                    break;
+                case ALPHA_RELATIVE:
+                    updateAlphaRelative(p.getValues());
+                    break;
+                case BATTERY:
+                    fileWriter.addDataPacket(1, p);
+                    // It's library client responsibility to flush the buffer,
+                    // otherwise you may get memory overflow.
+                    if (fileWriter.getBufferedMessagesSize() > 8096)
+                        fileWriter.flush();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void receiveMuseArtifactPacket(MuseArtifactPacket p) {
+            if (p.getHeadbandOn() && p.getBlink()) {
+                Log.i("Artifacts", "blink");
+            }
+        }
+
+        private void updateAccelerometer(final ArrayList<Double> data) {
+            EEGFragment fragment = fragmentRef.get();
+            if (fragment != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+        }
+
+        private void updateEeg(final ArrayList<Double> data) {
+            EEGFragment fragment = fragmentRef.get();
+            if (fragment != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+        }
+
+        private void updateAlphaRelative(final ArrayList<Double> data) {
+            EEGFragment fragment = fragmentRef.get();
+            if (fragment != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView elem1 = (TextView) getView().findViewById(R.id.elem1);
+                        TextView elem2 = (TextView) getView().findViewById(R.id.elem2);
+                        TextView elem3 = (TextView) getView().findViewById(R.id.elem3);
+                        TextView elem4 = (TextView) getView().findViewById(R.id.elem4);
+
+                        elem1.setText(String.format(
+                                "%6.2f", data.get(Eeg.TP9.ordinal())));
+                        elem2.setText(String.format(
+                                "%6.2f", data.get(Eeg.FP1.ordinal())));
+                        elem3.setText(String.format(
+                                "%6.2f", data.get(Eeg.FP2.ordinal())));
+                        elem4.setText(String.format(
+                                "%6.2f", data.get(Eeg.TP10.ordinal())));
+                    }
+                });
+            }
+        }
+
+        public void setFileWriter(MuseFileWriter fileWriter) {
+            this.fileWriter  = fileWriter;
+        }
+    }
     public static EEGFragment newInstance(String param1, String param2) {
         EEGFragment fragment = new EEGFragment();
         return fragment;
     }
 
     public EEGFragment() {
-        // Required empty public constructor
+        // Create listeners and pass reference to activity to them
+        WeakReference<EEGFragment> weakFragment =
+                new WeakReference<EEGFragment>(this);
+
+        connectionListener = new ConnectionListener(weakFragment);
+        dataListener = new DataListener(weakFragment);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Spinner musesSpinner = (Spinner) getView().findViewById(R.id.muses_spinner);
+        if (v.getId() == R.id.refresh) {
+            MuseManager.refreshPairedMuses();
+            List<Muse> pairedMuses = MuseManager.getPairedMuses();
+            List<String> spinnerItems = new ArrayList<String>();
+            for (Muse m: pairedMuses) {
+                String dev_id = m.getName() + "-" + m.getMacAddress();
+                Log.i("Muse Headband", dev_id);
+                spinnerItems.add(dev_id);
+            }
+            ArrayAdapter<String> adapterArray = new ArrayAdapter<String> (getActivity(), android.R.layout.simple_spinner_item, spinnerItems);
+            musesSpinner.setAdapter(adapterArray);
+        }
+        else if (v.getId() == R.id.connect) {
+            List<Muse> pairedMuses = MuseManager.getPairedMuses();
+            if (pairedMuses.size() < 1 ||
+                    musesSpinner.getAdapter().getCount() < 1) {
+                Log.w("Muse Headband", "There is nothing to connect to");
+            }
+            else {
+                muse = pairedMuses.get(musesSpinner.getSelectedItemPosition());
+                ConnectionState state = muse.getConnectionState();
+                if (state == ConnectionState.CONNECTED ||
+                        state == ConnectionState.CONNECTING) {
+                    Log.w("Muse Headband",
+                            "doesn't make sense to connect second time to the same muse");
+                    return;
+                }
+                configureLibrary();
+                fileWriter.open();
+                fileWriter.addAnnotationString(1, "Connect clicked");
+                /**
+                 * In most cases libmuse native library takes care about
+                 * exceptions and recovery mechanism, but native code still
+                 * may throw in some unexpected situations (like bad bluetooth
+                 * connection). Print all exceptions here.
+                 */
+                try {
+                    muse.runAsynchronously();
+                } catch (Exception e) {
+                    Log.e("Muse Headband", e.toString());
+                }
+            }
+        }
+        else if (v.getId() == R.id.disconnect) {
+            if (muse != null) {
+                /**
+                 * true flag will force libmuse to unregister all listeners,
+                 * BUT AFTER disconnecting and sending disconnection event.
+                 * If you don't want to receive disconnection event (for ex.
+                 * you call disconnect when application is closed), then
+                 * unregister listeners first and then call disconnect:
+                 * muse.unregisterAllListeners();
+                 * muse.disconnect(false);
+                 */
+                muse.disconnect(true);
+                fileWriter.addAnnotationString(1, "Disconnect clicked");
+                fileWriter.flush();
+                fileWriter.close();
+            }
+        }
+        else if (v.getId() == R.id.pause) {
+            dataTransmission = !dataTransmission;
+            if (muse != null) {
+                muse.enableDataTransmission(dataTransmission);
+            }
+        }
+    }
+
+    private void playMuseFile(String name) {
+        File dir = getActivity().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(dir, name);
+        final String tag = "Muse File Reader";
+        if (!file.exists()) {
+            Log.w(tag, "file doesn't exist");
+            return;
+        }
+        MuseFileReader fileReader = MuseFileFactory.getMuseFileReader(file);
+        while (fileReader.gotoNextMessage()) {
+            MessageType type = fileReader.getMessageType();
+            int id = fileReader.getMessageId();
+            long timestamp = fileReader.getMessageTimestamp();
+            Log.i(tag, "type: " + type.toString() +
+                    " id: " + Integer.toString(id) +
+                    " timestamp: " + String.valueOf(timestamp));
+            switch(type) {
+                case EEG: case BATTERY: case ACCELEROMETER: case QUANTIZATION:
+                    MuseDataPacket packet = fileReader.getDataPacket();
+                    Log.i(tag, "data packet: " + packet.getPacketType().toString());
+                    break;
+                case VERSION:
+                    MuseVersion version = fileReader.getVersion();
+                    Log.i(tag, "version" + version.getFirmwareType());
+                    break;
+                case CONFIGURATION:
+                    MuseConfiguration config = fileReader.getConfiguration();
+                    Log.i(tag, "config" + config.getBluetoothMac());
+                    break;
+                case ANNOTATION:
+                    AnnotationData annotation = fileReader.getAnnotation();
+                    Log.i(tag, "annotation" + annotation.getData());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    private void configureLibrary() {
+        muse.registerConnectionListener(connectionListener);
+        muse.registerDataListener(dataListener,
+                MuseDataPacketType.ACCELEROMETER);
+        muse.registerDataListener(dataListener,
+                MuseDataPacketType.EEG);
+        muse.registerDataListener(dataListener,
+                MuseDataPacketType.ALPHA_RELATIVE);
+        muse.registerDataListener(dataListener,
+                MuseDataPacketType.ARTIFACTS);
+        muse.registerDataListener(dataListener,
+                MuseDataPacketType.BATTERY);
+        muse.setPreset(MusePreset.PRESET_14);
+        muse.enableDataTransmission(dataTransmission);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eeg, container, false);
+        View view = inflater.inflate(R.layout.fragment_eeg, container, false);
+
+        Button refreshButton = (Button) view.findViewById(R.id.refresh);
+        refreshButton.setOnClickListener(this);
+        Button connectButton = (Button) view.findViewById(R.id.connect);
+        connectButton.setOnClickListener(this);
+        Button disconnectButton = (Button) view.findViewById(R.id.disconnect);
+        disconnectButton.setOnClickListener(this);
+        Button pauseButton = (Button) view.findViewById(R.id.pause);
+        pauseButton.setOnClickListener(this);
+        // // Uncommet to test Muse File Reader
+        //
+        // // file can be big, read it in a separate thread
+        // Thread thread = new Thread(new Runnable() {
+        //     public void run() {
+        //         playMuseFile("testfile.muse");
+        //     }
+        // });
+        // thread.start();
+
+        File dir = view.getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        fileWriter = MuseFileFactory.getMuseFileWriter(
+                new File(dir, "new_muse_file.muse"));
+        Log.i("Muse Headband", "libmuse version=" + LibMuseVersion.SDK_VERSION);
+        fileWriter.addAnnotationString(1, "MainActivity onCreate");
+        dataListener.setFileWriter(fileWriter);
+        index = 0;
+        full = false;
+        alpha = new double[1000];
+        beta= new double[1000];
+        gamma= new double[1000];
+        theta= new double[1000];
+        return view;
     }
 
 
